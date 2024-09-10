15bf39a21185fb32d3e6b538c83ec661cf198e16
Rohit Mukkala <mukkalarohit@gmail.com>
2024-09-10 11:24:03 +0530

    Project SetUp

---
+++ script.js
@@ -0,0 +1,18 @@
+var playeRed = "R";
+playeryellow = "y";
+
+var currPlayer = playeRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [];
+}
